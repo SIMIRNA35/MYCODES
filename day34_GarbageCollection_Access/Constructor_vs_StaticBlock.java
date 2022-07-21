1c4cae37188b34aa3240808aaad60bccfba00f9e
Metin <metinoruc3@gmail.com>
2022-07-21 23:31:35 +0200

    day34_GarbageCollection_Access

--- day34_GarbageCollection_Access/Constructor_vs_StaticBlock.java
+++ day34_GarbageCollection_Access/Constructor_vs_StaticBlock.java
@@ -10,6 +10,9 @@ public class Constructor_vs_StaticBlock {
     }
 
     public static void main(String[] args) {
-        System.out.println("Main Method");
+        new Constructor_vs_StaticBlock(); //1
+        new Constructor_vs_StaticBlock(); //2
+        new Constructor_vs_StaticBlock(); //3
+        new Constructor_vs_StaticBlock(); //4
     }
 }
