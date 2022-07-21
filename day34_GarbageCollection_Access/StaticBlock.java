a0248a19d05d9dd2e8fc4eba7c5f9829f876f4a2
Metin <metinoruc3@gmail.com>
2022-07-21 23:21:59 +0200

    day34

---
+++ day34_GarbageCollection_Access/StaticBlock.java
@@ -0,0 +1,25 @@
+package day34_GarbageCollection_Access;
+
+public class StaticBlock {
+    public static void main(String[] args) {
+
+        System.out.println("Main Method");
+
+
+
+
+
+    }
+
+    static { // runs first before anything , and only runs one time
+        System.out.println("Static Block");
+
+
+    }
+
+
+
+
+
+
+}
---
+++ day34_GarbageCollection_Access/Test_Circle.java
@@ -0,0 +1,15 @@
+package day34_GarbageCollection_Access;
+
+public class Test_Circle {
+
+    public static void main(String[] args) {
+
+        System.out.println(Circle.pi);
+        System.out.println(Circle.name);
+        System.out.println(Circle.numbers);
+    }
+
+
+
+
+}
