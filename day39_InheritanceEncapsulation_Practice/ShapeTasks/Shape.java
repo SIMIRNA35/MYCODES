b19e848df560d06de9c6271cf38bbeb66905fb28
Metin <metinoruc3@gmail.com>
2022-08-01 12:33:04 +0200

    day39 Inheritance - Encapsulation Practices

--- day39_InheritanceEncapsulation_Practice/ShapeTasks/Shape.java
+++ day39_InheritanceEncapsulation_Practice/ShapeTasks/Shape.java
@@ -31,7 +31,14 @@ public class Shape {
       return 0;
     }
 
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
     }
+}
 
 /*
 Shape:
